d638ae521e72f5ccc7883ab87a7beb9013a1ca59
Mikhail Tsay <37892513+MkTsNix@users.noreply.github.com>
2023-01-09 15:26:43 +0600

    Update game.sh

--- game.sh
+++ game.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\\n NumberJack \\n"
+echo -e "\n NumberJack \n"
 ch=0
 while [ "$ch" -ne 3 ]; do
     echo  "
@@ -8,10 +8,10 @@ while [ "$ch" -ne 3 ]; do
          HELP : Hit 2 and enter.
          EXIT : Hit 3 and enter.
          "
-    read -p -r "Enter your choice : " ch
+    read -rp "Enter your choice : " ch
     if [ "$ch" -eq 1 ];then
     x=0 ;c=0 ;p=0
-    read -p -r "Enter any number between 0 and 9 : " n
+    read -rp "Enter any number between 0 and 9 : " n
     while [ $c -eq 0 ];
     do
         x=11; r="$(shuf -i 0-9 -n 10)"
@@ -19,7 +19,7 @@ while [ "$ch" -ne 3 ]; do
             a["$i"]=$i
         done
         echo "${a[*]} "
-        read -t -r 5 -p "Enter the index of your number : " x
+        read -t 5 -rp "Enter the index of your number : " x
     if [[ $? -gt 128 ]]; then
     c=1
     break
